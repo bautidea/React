44a1b6ed0aa8b0c6aa0785f1199f725ff237d153
bautidea <juanbautistadeangelis@gmail.com>
2023-11-08 18:12:17 -0300

    Added Form Validation with zod.

--- react-project/src/components/AddExpenseForm.tsx
+++ react-project/src/components/AddExpenseForm.tsx
@@ -1,14 +1,31 @@
-import React, { FormEvent } from 'react';
 import { FieldValues, useForm } from 'react-hook-form';
-
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 interface Props {
   categories: string[];
   onSubmit: (data: FieldValues) => void;
 }
 
+const schema = z.object({
+  description: z
+    .string()
+    .min(3, { message: 'Name must be at least 3 characters.' }),
+  amount: z.number({
+    required_error: 'Amount is required.',
+    invalid_type_error: 'Age must be a number.',
+  }),
+  category: z.string().min(1, { message: 'Category is required' }),
+});
+
+type FormData = z.infer<typeof schema>;
+
 const AddExpenseForm = ({ categories, onSubmit }: Props) => {
   // Using react-hook-form to handle form submissions
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -23,6 +40,10 @@ const AddExpenseForm = ({ categories, onSubmit }: Props) => {
           className="form-control"
           type="text"
         />
+
+        {errors.description && (
+          <p className="text-danger">{errors.description.message}</p>
+        )}
       </div>
 
       <div className="mb-3">
@@ -31,11 +52,15 @@ const AddExpenseForm = ({ categories, onSubmit }: Props) => {
         </label>
 
         <input
-          {...register('amount')}
+          {...register('amount', { valueAsNumber: true })}
           id="amount"
           className="form-control"
-          type="text"
+          type="number"
         />
+
+        {errors.amount && (
+          <p className="text-danger">{errors.amount.message}</p>
+        )}
       </div>
 
       <div className="mb-3">
@@ -47,9 +72,9 @@ const AddExpenseForm = ({ categories, onSubmit }: Props) => {
           {...register('category')}
           id="category"
           className="form-select"
-          defaultValue={'default'}
+          defaultValue={''}
         >
-          <option value="default" disabled></option>
+          <option value="" disabled></option>
 
           {categories.map((item, ind) => (
             <option key={ind} value={item}>
@@ -57,6 +82,10 @@ const AddExpenseForm = ({ categories, onSubmit }: Props) => {
             </option>
           ))}
         </select>
+
+        {errors.category && (
+          <p className="text-danger">{errors.category.message}</p>
+        )}
       </div>
 
       <button type="submit" className="btn btn-primary mb-5">
