c5f785b3aef30341df5aed58b0fb65b4d4f43818
bautidea <juanbautistadeangelis@gmail.com>
2023-11-12 14:04:33 -0300

    Finish.

--- react_begginer/6_connecting_to_backend/10_creating_data/App.tsx
+++ react_begginer/6_connecting_to_backend/10_creating_data/App.tsx
@@ -7,6 +7,7 @@ interface User {
 }
 
 function App() {
+  // Lets see how we can add a user.
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState('');
   const [isLoading, setLoading] = useState(false);
@@ -49,14 +50,22 @@ function App() {
 
   // Function for adding a user.
   const addUser = () => {
+    // Constant to save original list in case an error occur.
     const originalUsers = [...users];
 
+    // We are going to use an Optimistic Update.
+    // In a real app the values of the new user would be based on the values on a form
+    // but we dont have a form here, we are hardcoding them.
     const newUser = { id: 0, name: 'Mosh' };
 
     setUsers([newUser, ...users]);
 
+    // Calling server to save the changes, and passing the new user object.
     axios
       .post('https://jsonplaceholder.typicode.com/users/' + newUser)
+      // If the call is successful we should refresh the list with the saved user
+      // because the new user has an ID that is going to be generated on the server.
+      //* .then((res) => setUsers([res.data, ...users])); --> Restructuring
       .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
       .catch((err) => {
         setError(err.message);
